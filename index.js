85a1bd6392bb1938b2e1d536ea7723f5725a292f
zuimengcf <zuimengqm@gmail.com>
2026-06-02 22:58:30 +0800

    use sample_url for better quality

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get("/", async (req, res) => {
     // Yande.re 随机获取一张图片
     const r = await axios.get("https://yande.re/post.json?limit=1&tags=rating:safe order:random", {timeout: 10000});
     if (r.data && r.data[0]) {
-      // 使用预览图或原图
-      const imgUrl = r.data[0].preview_url || r.data[0].file_url;
+      // 使用高质量预览图 (约 850px)
+      const imgUrl = r.data[0].sample_url;
       res.redirect(imgUrl);
     } else {
       res.status(404).send("No image found");
